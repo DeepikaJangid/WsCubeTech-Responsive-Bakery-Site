59e7550e18c07d664f8b48cc858f67ff80a92caa
DeepikaJangid <deepikajangid021@gmail.com>
2025-09-29 18:46:06 +0530

    Tablet responsive

--- js/app.js
+++ js/app.js
@@ -131,3 +131,45 @@ pauseBtn.addEventListener(
     }
 )
 
+// review section
+// should be a node list for foreach to work... it will not work on htmlcollection
+
+// slick slider
+
+$('.reviews').slick({
+    dots: true,
+    infinite: true,
+    speed: 900,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
